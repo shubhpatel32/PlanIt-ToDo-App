3c99aebcc20f590c7e572e69f8f3f3845173e549
Shubh <sdpatel972@gmail.com>
2025-02-19 22:14:46 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,42 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cron = require("node-cron");
 const { checkTasksAndSendEmails } = require("./cron/taskReminder");
+
 const app = express();
 app.use(express.json());
 const API_URL = process.env.FRONTEND_URL;
 
-app.use(
-  cors({
-    origin: API_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    API_URL || "https://planit-frontend-swart.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
 app.use("/api/auth", require("./routes/userRoutes"));
 app.use("/api/todos", require("./routes/todoRoutes"));
 
