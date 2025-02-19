80633002193656e756dfc6ce56a8f3fb21ccedf5
Shubh <sdpatel972@gmail.com>
2025-02-19 21:45:54 +0530

    vercel.json updated

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ const cron = require("node-cron");
 const { checkTasksAndSendEmails } = require("./cron/taskReminder");
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+//     credentials: true,
+//   })
+// );
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -20,6 +20,19 @@ mongoose
     console.log("MongoDB Connected");
   })
   .catch((err) => console.log("MongoDB Connection Error:", err));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use("/api/auth", require("./routes/userRoutes"));
 app.use("/api/todos", require("./routes/todoRoutes"));
