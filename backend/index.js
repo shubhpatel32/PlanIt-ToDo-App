3f3fc9729a51c94ebdbd347d418dc0981d976e71
Shubh <sdpatel972@gmail.com>
2025-02-19 21:34:46 +0530

    vercel.json updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
   })
